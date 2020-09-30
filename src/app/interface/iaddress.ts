116c182e266d0fcc74ae48c45619820aae4a7189
ZhengxiLI <57601687+ZhnegxiLI@users.noreply.github.com>
2020-09-30 17:57:44 +0200

    change the facturation modification logic, shipping address always not correcte, please don't publish this version

---
+++ src/app/interface/iaddress.ts
@@ -0,0 +1,13 @@
+export interface Iaddress {
+    Id: number,
+    EntrepriseName: string,
+    ContactFirstName: string,
+    ContactLastName: string,
+    FirstLineAddress: string,
+    SecondLineAddress?: string,
+    City: string,
+    Country: string,
+    ZipCode: string,
+    ContactTelephone: string,
+    ContactFax?: string
+}
