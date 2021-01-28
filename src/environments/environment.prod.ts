ae7b779a020c26f055cc3442724f94b02f784345
zhengxili <zhengxi.li.19@neoma-bs.com>
2021-01-28 21:48:48 +0100

    Dev: add new dev for mobile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  SERVER_API_URL: 'https://jlsprod.europetechs.com/',
+  SERVER_API_URL: 'https://www.jlsimport.fr/',
   HTTP_WAITING_TIME: 20000, // ms
 };
